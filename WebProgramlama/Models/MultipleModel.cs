ed35469689c6130226902f5a141c91d86db9b936
betulmetin <betulmetin718@gmail.com>
2018-07-11 09:10:02 +0300

    Update MultipleModel.cs

--- WebProgramlama/Models/MultipleModel.cs
+++ WebProgramlama/Models/MultipleModel.cs
@@ -9,10 +9,9 @@ using WebProgramlama.Models;
 namespace WebProgramlama.Models
 {
     public class MultipleModel
-   {
-
-        
+   {        
        public List<Urunler>urunler {get; set;}
+        
        public List<Uyeler> uyeler { get; set; }
 
        public List<Urunler> tumUrunler { get; set; }
